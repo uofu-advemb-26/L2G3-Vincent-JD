57d0c5e82088f12db82a8a04d0486e2590199d1f
Vincent <vincent.themajortechie@gmail.com>
2026-09-16 15:07:22 -0600

    Basic printf and a blank test successfully implemented and passing.

--- test/test.c
+++ test/test.c
@@ -9,16 +9,16 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-// void test_LED(void) {
-//     printf("Starting LED test\n");
-//     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false); 
-//     printf("Attempting to force LED off\n");
-//     int gpio = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN); 
-//     TEST_ASSERT_MESSAGE(gpio == 0, "LED successfully turned off");
-//     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true); 
-//     gpio = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN); 
-//     TEST_ASSERT_MESSAGE(gpio == 1, "LED successfully turned on");
-// }
+void test_LED(void) {
+    printf("Starting LED test\n");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false); 
+    printf("Attempting to force LED off\n");
+    int gpio = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN); 
+    TEST_ASSERT_MESSAGE(gpio == 0, "LED successfully turned off");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true); 
+    gpio = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN); 
+    TEST_ASSERT_MESSAGE(gpio == 1, "LED successfully turned on");
+}
 
 void test_variable_assignment()
 {
@@ -34,6 +34,10 @@ void test_multiplication(void)
     TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
 }
 
+void blank_test(void) {
+    printf("This is a test. If you see this message then the blank test has succeeded.\n");
+}
+
 int main (void)
 {
     stdio_init_all();
@@ -44,7 +48,8 @@ int main (void)
         RUN_TEST(test_variable_assignment);
         RUN_TEST(test_multiplication);
         fflush(stdout);
-        // printf("End Tests\n");
+        printf("End Tests\n");
+        RUN_TEST(blank_test);
         // RUN_TEST(test_LED);
         // sleep_ms(5000);
         UNITY_END();
